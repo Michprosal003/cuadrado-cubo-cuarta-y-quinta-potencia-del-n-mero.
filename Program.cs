744e49951f7ed4f1ede17814006e52a3eb4ad915
Programador Michel Rosales <95501615+Michprosal003@users.noreply.github.com>
2022-03-18 14:54:34 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero, resultado;
+
+
+            Console.Title = "Elevar un número al cuadrado";
+
+            Console.WriteLine("Ingresar un numero");
+            numero = int.Parse(Console.ReadLine());
+
+            resultado = numero * numero;
+
+
+            Console.WriteLine(" El numero " + numero + " elevado al cuadrado es :" + resultado);
+            Console.ReadKey();
+
+        }
+    }
+}
